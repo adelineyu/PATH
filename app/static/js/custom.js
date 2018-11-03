762fdd386513d8599aa0d3c7ceb9a23263c925a5
xanderkoo <acka2017@pom.edu>
2018-11-03 01:48:02 -0700

    shit

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -7,6 +7,10 @@
 
 var directionsService;
 var directionsDisplay;
+var inputFrom;
+var searchBoxFrom;
+var inputTo;
+var searchBoxTo;
 
 function initMap() {
 
@@ -21,114 +25,132 @@ function initMap() {
   });
 
   // Create the search box and link it to the UI element.
-  var inputFrom = document.getElementById('pac-input-from');
-  var searchBoxFrom = new google.maps.places.SearchBox(inputFrom);
-  var inputTo = document.getElementById('pac-input-to');
-  var searchBoxTo = new google.maps.places.SearchBox(inputTo);
+  inputFrom = document.getElementById('pac-input-from');
+  searchBoxFrom = new google.maps.places.SearchBox(inputFrom);
+  inputTo = document.getElementById('pac-input-to');
+  searchBoxTo = new google.maps.places.SearchBox(inputTo);
 
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
     searchBoxFrom.setBounds(map.getBounds());
     searchBoxTo.setBounds(map.getBounds());
   });
+
+  var markerFrom;
+  var markerTo;
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBoxFrom.addListener('places_changed', function() {
+    var places = searchBoxFrom.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      if(markerFrom != null) {
+        markerFrom.setMap(null);
+      }
+
+      // Create a marker for each place.
+      markerFrom = new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+
+      if (markerTo != null) {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+      }
+    });
+  });
+
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBoxTo.addListener('places_changed', function() {
+    var places = searchBoxTo.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      if(markerTo != null) {
+        markerTo.setMap(null);
+      }
+
+      // Create a marker for each place.
+      markerTo = new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+
+      if (markerFrom != null) {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+      }
+
+    });
+  });
 }
 
-var markerFrom;
-var markerTo;
-// Listen for the event fired when the user selects a prediction and retrieve
-// more details for that place.
-searchBoxFrom.addListener('places_changed1', function() {
-  var places = searchBoxFrom.getPlaces();
 
-  if (places.length == 0) {
-    return;
-  }
 
-  // For each place, get the icon, name and location.
-  var bounds = new google.maps.LatLngBounds();
-  places.forEach(function(place) {
-    if (!place.geometry) {
-      console.log("Returned place contains no geometry");
-      return;
-    }
-    var icon = {
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(25, 25)
-    };
 
-    if(markerFrom != null) {
-      markerFrom.setMap(null);
-    }
 
-    // Create a marker for each place.
-    markerFrom = new google.maps.Marker({
-      map: map,
-      icon: icon,
-      title: place.name,
-      position: place.geometry.location
-    });
-
-    if (place.geometry.viewport) {
-      // Only geocodes have viewport.
-      bounds.union(place.geometry.viewport);
-    } else {
-      bounds.extend(place.geometry.location);
-    }
-  });
-});
-
-// Listen for the event fired when the user selects a prediction and retrieve
-// more details for that place.
-searchBoxTo.addListener('places_changed2', function() {
-  var places = searchBoxTo.getPlaces();
-
-  if (places.length == 0) {
-    return;
-  }
-
-  // For each place, get the icon, name and location.
-  var bounds = new google.maps.LatLngBounds();
-  places.forEach(function(place) {
-    if (!place.geometry) {
-      console.log("Returned place contains no geometry");
-      return;
-    }
-    var icon = {
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(25, 25)
-    };
-
-    if(markerTo != null) {
-      markerTo.setMap(null);
-    }
-
-    // Create a marker for each place.
-    markerTo = new google.maps.Marker({
-      map: map,
-      icon: icon,
-      title: place.name,
-      position: place.geometry.location
-    });
-
-    if (place.geometry.viewport) {
-      // Only geocodes have viewport.
-      bounds.union(place.geometry.viewport);
-    } else {
-      bounds.extend(place.geometry.location);
-    }
-  });
-});
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  window.alert(searchBoxFrom.getPlaces() + " " + searchBoxTo.getPlaces());
+
   directionsService.route({
-    origin: document.getElementById('searchBoxFrom').value,
-    destination: document.getElementById('searchBoxTo').value,
+    origin: inputFrom.value,
+    // document.getElementById('pac-input-from').value,
+    destination: inputTo.value,
+    // document.getElementById('pac-input-to').value,
     travelMode: 'DRIVING'
   }, function(response, status) {
     if (status === 'OK') {
@@ -139,7 +161,7 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   });
 }
 
-var button = getElementById('find-button')
-button.addListener('button-clicked', function() {
-  calculateAndDisplayRoute(directionsService, directionsDisplay);
-});
+// var button = document.getElementById('find-button')
+// button.addEventListener('click', function() {
+//   calculateAndDisplayRoute(directionsService, directionsDisplay);
+// });
