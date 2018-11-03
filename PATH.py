4f5007a742409a28368b28a6445753bfdd469426
xanderkoo <acka2017@pom.edu>
2018-11-02 20:24:44 -0700

    uber price estimate function in PATH.py

--- PATH.py
+++ PATH.py
@@ -1,1 +1,19 @@
 from app import app
+
+# before running this file, run
+#   pip install uber-rides
+# in command line
+from uber_rides.session import Session
+from uber_rides.client import UberRidesClient
+
+session = Session(server_token='ZaorUL2L_CCILlRtjHBrLz93-hTPIIBJHExq4C5m')
+client = UberRidesClient(session)
+
+def price_estimate(slat, slong, elat, elong, seats):
+    response = client.get_price_estimates(
+        start_latitude=slat,
+        start_longitude=-slong,
+        end_latitude=elat,
+        end_longitude=-elong,
+        seat_count=seats
+    )
